63572132145dd09ae7c1d0e2db56078b7d08423c
Luciano Weslen da Silva <luciano.weslen11@gmail.com>
2019-07-22 18:52:39 -0300

    Code

---
+++ Code/FollowLine/FollowLine.ino
@@ -0,0 +1,17 @@
+/*	#############################################
+	# 			ROBÔ DE RESGATE (OBR)			#
+	#			 EQUIPE ROBOTIC4ALL				#
+	#					2019					#
+	#############################################  */
+
+/* INCLUINDO BIBLIOTECAS */
+#include <QTRSensors.h>
+
+
+void setup(void){
+
+}
+
+void loop(void){
+	rescueLine();
+}
